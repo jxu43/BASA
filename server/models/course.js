49e74556ff70e1d59d1521d6fc296c6042dcb656
Jiahao Xu <xualex2018@gmail.com>
2019-04-16 03:49:39 -0400

    backend init

---
+++ server/models/course.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+
+var CourseSchema = new mongoose.Schema({
+    courseid: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    subject: String,
+    educatorId: String,
+    sections: [
+        {
+            sectionId: {type: String},
+            video: {type: Buffer},
+            description: {type: String},
+            comments: [{
+                comment: Comment
+            }]
+        }]
+})
+
+var Course = mongoose.model("course", CourseSchema)
+module.exports = {
+    Course: Course
+}
